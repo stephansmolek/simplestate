dcaeac472c1cc5abd4432eec5871cb4e8283df94
Stephan Smolek <stephansmolek@gmail.com>
2023-02-22 23:15:15 +0100

    Remove testing dependencies.

--- simplestate_test.go
+++ simplestate_test.go
@@ -2,8 +2,6 @@ package simplestate
 
 import (
 	"testing"
-
-	"github.com/stretchr/testify/assert"
 )
 
 var (
@@ -64,9 +62,17 @@ func TestNewMachine(t *testing.T) {
 		t.Error("NewMachine returned nil")
 	}
 	if m != nil {
-		assert.Equal(t, testStateA, m.currentState)
-		assert.Equal(t, 3, len(m.events))
-		assert.Equal(t, settings, m.settings)
+		if m.currentState != testStateA {
+			t.Errorf("NewMachine returned invalid state: %v", m.currentState)
+		}
+
+		if len(m.events) != 3 {
+			t.Errorf("NewMachine returned invalid events: %v", m.events)
+		}
+
+		if m.settings != settings {
+			t.Errorf("NewMachine returned invalid settings: %v", m.settings)
+		}
 	}
 }
 
@@ -82,8 +88,8 @@ func TestGet(t *testing.T) {
 	if m == nil {
 		t.Error("NewMachine returned nil")
 	}
-	if m != nil {
-		assert.Equal(t, testStateA, m.Get())
+	if m != nil && m.Get() != testStateA {
+		t.Errorf("Get() returned invalid state: %v", m.Get())
 	}
 }
 
@@ -97,8 +103,12 @@ func TestIs(t *testing.T) {
 		t.Error("NewMachine returned nil")
 	}
 	if m != nil {
-		assert.True(t, m.Is(testStateA))
-		assert.False(t, m.Is(testStateB))
+		if !m.Is(testStateA) {
+			t.Errorf("Is() returned invalid state: %v", m.Is(testStateA))
+		}
+		if m.Is(testStateB) {
+			t.Errorf("Is() returned invalid state: %v", m.Is(testStateB))
+		}
 	}
 }
 
@@ -113,7 +123,9 @@ func TestSet(t *testing.T) {
 	}
 	if m != nil {
 		m.Set(testStateB)
-		assert.Equal(t, testStateB, m.Get())
+		if m.Get() != testStateB {
+			t.Errorf("Invalid state after Set(): %v", m.Get())
+		}
 	}
 }
 
@@ -127,9 +139,17 @@ func TestCan(t *testing.T) {
 		t.Error("NewMachine returned nil")
 	}
 	if m != nil {
-		assert.True(t, m.Can(testEventId1))
-		assert.False(t, m.Can(testEventId2))
-		assert.False(t, m.Can(testEventId3))
+		if !m.Can(testEventId1) {
+			t.Errorf("Can() true expected for event: %v", testEventId1)
+		}
+
+		if m.Can(testEventId2) {
+			t.Errorf("Can() false expected for event: %v", testEventId2)
+		}
+
+		if m.Can(testEventId3) {
+			t.Errorf("Can() false expected for event: %v", testEventId3)
+		}
 	}
 }
 
@@ -143,7 +163,9 @@ func TestCanWithInvalidEvent(t *testing.T) {
 		t.Error("NewMachine returned nil")
 	}
 	if m != nil {
-		assert.False(t, m.Can("invalid-event"))
+		if m.Can("invalid-event") {
+			t.Errorf("Can() false expected for invalid event")
+		}
 	}
 }
 
@@ -157,7 +179,9 @@ func TestCanWithInvalidState(t *testing.T) {
 		t.Error("NewMachine returned nil")
 	}
 	if m != nil {
-		assert.False(t, m.Can(testEventId2))
+		if m.Can(testEventId2) {
+			t.Errorf("Can() false expected for event: %v", testEventId2)
+		}
 	}
 }
 
@@ -171,7 +195,9 @@ func TestCanWithSameStateTransition(t *testing.T) {
 		t.Error("NewMachine returned nil")
 	}
 	if m != nil {
-		assert.True(t, m.Can(testEventId3))
+		if !m.Can(testEventId3) {
+			t.Errorf("Can() true expected for event: %v", testEventId3)
+		}
 	}
 }
 
@@ -185,9 +211,17 @@ func TestTrigger(t *testing.T) {
 		t.Error("NewMachine returned nil")
 	}
 	if m != nil {
-		assert.True(t, m.Can(testEventId1))
-		assert.NoError(t, m.Trigger(testEventId1))
-		assert.Equal(t, testStateC, m.Get())
+		if !m.Can(testEventId1) {
+			t.Errorf("Can() true expected for event: %v", testEventId1)
+		}
+
+		if m.Trigger(testEventId1) != nil {
+			t.Errorf("Trigger() returned error for event: %v", testEventId1)
+		}
+
+		if m.Get() != testStateC {
+			t.Errorf("Invalid state after Trigger(): %v", m.Get())
+		}
 	}
 }
 
@@ -201,9 +235,17 @@ func TestTriggerWithInvalidEvent(t *testing.T) {
 		t.Error("NewMachine returned nil")
 	}
 	if m != nil {
-		assert.False(t, m.Can("invalid-event"))
-		assert.Error(t, m.Trigger("invalid-event"))
-		assert.Equal(t, testStateA, m.Get())
+		if m.Can("invalid-event") {
+			t.Errorf("Can() false expected for invalid event")
+		}
+
+		if m.Trigger("invalid-event") == nil {
+			t.Errorf("Trigger() returned no error for invalid event")
+		}
+
+		if m.Get() != testStateA {
+			t.Errorf("Invalid state after Trigger(): %v", m.Get())
+		}
 	}
 }
 
@@ -217,9 +259,17 @@ func TestTriggerWithInvalidState(t *testing.T) {
 		t.Error("NewMachine returned nil")
 	}
 	if m != nil {
-		assert.False(t, m.Can(testEventId2))
-		assert.Error(t, m.Trigger(testEventId2))
-		assert.Equal(t, testStateA, m.Get())
+		if m.Can(testEventId2) {
+			t.Errorf("Can() false expected for event: %v", testEventId2)
+		}
+
+		if m.Trigger(testEventId2) == nil {
+			t.Errorf("Trigger() returned no error for invalid event")
+		}
+
+		if m.Get() != testStateA {
+			t.Errorf("Invalid state after Trigger(): %v", m.Get())
+		}
 	}
 }
 
@@ -233,9 +283,17 @@ func TestTriggerWithSameStateTransition(t *testing.T) {
 		t.Error("NewMachine returned nil")
 	}
 	if m != nil {
-		assert.True(t, m.Can(testEventId3))
-		assert.NoError(t, m.Trigger(testEventId3))
-		assert.Equal(t, testStateA, m.Get())
+		if !m.Can(testEventId3) {
+			t.Errorf("Can() true expected for event: %v", testEventId3)
+		}
+
+		if m.Trigger(testEventId3) != nil {
+			t.Errorf("Trigger() returned error for event: %v", testEventId3)
+		}
+
+		if m.Get() != testStateA {
+			t.Errorf("Invalid state after Trigger(): %v", m.Get())
+		}
 	}
 }
 
@@ -249,9 +307,17 @@ func TestTriggerWithSameStateTransitionDisallowed(t *testing.T) {
 		t.Error("NewMachine returned nil")
 	}
 	if m != nil {
-		assert.False(t, m.Can(testEventId3))
-		assert.Error(t, m.Trigger(testEventId3))
-		assert.Equal(t, testStateA, m.Get())
+		if m.Can(testEventId3) {
+			t.Errorf("Can() false expected for event: %v", testEventId3)
+		}
+
+		if m.Trigger(testEventId3) == nil {
+			t.Errorf("Trigger() returned no error for invalid event")
+		}
+
+		if m.Get() != testStateA {
+			t.Errorf("Invalid state after Trigger(): %v", m.Get())
+		}
 	}
 }
 
@@ -265,25 +331,54 @@ func TestTriggerWithInvalidTransition(t *testing.T) {
 		t.Error("NewMachine returned nil")
 	}
 	if m != nil {
-		assert.False(t, m.Can(testEventId2))
-		assert.Error(t, m.Trigger(testEventId2))
-		assert.Equal(t, testStateA, m.Get())
+		if m.Can(testEventId2) {
+			t.Errorf("Can() false expected for event: %v", testEventId2)
+		}
+
+		if m.Trigger(testEventId2) == nil {
+			t.Errorf("Trigger() returned no error for invalid event")
+		}
+
+		if m.Get() != testStateA {
+			t.Errorf("Invalid state after Trigger(): %v", m.Get())
+		}
 	}
 }
 
 func TestInvaildEventError(t *testing.T) {
 	err := &InvalidEventError[TestEvent]{"invalid-event"}
-	assert.Equal(t, "invalid event: invalid-event", err.Error())
+
+	if err == nil {
+		t.Error("InvalidEventError returned nil")
+	}
+
+	if err.Error() != "invalid event: invalid-event" {
+		t.Errorf("InvalidEventError returned invalid error: %v", err.Error())
+	}
 }
 
 func TestInvalidStateError(t *testing.T) {
 	err := &InvalidStateError[TestState]{testStateA}
-	assert.Equal(t, "invalid state: state-A", err.Error())
+
+	if err == nil {
+		t.Error("InvalidStateError returned nil")
+	}
+
+	if err.Error() != "invalid state: state-A" {
+		t.Errorf("InvalidStateError returned invalid error: %v", err.Error())
+	}
 }
 
 func TestSameStateTransitionError(t *testing.T) {
 	err := &SameStateTransitionError[TestState]{testStateA}
-	assert.Equal(t, "same state transition not allowed: state-A", err.Error())
+
+	if err == nil {
+		t.Error("SameStateTransitionError returned nil")
+	}
+
+	if err.Error() != "same state transition not allowed: state-A" {
+		t.Errorf("SameStateTransitionError returned invalid error: %v", err.Error())
+	}
 }
 
 func TestTriggerFromCallback(t *testing.T) {
@@ -293,7 +388,9 @@ func TestTriggerFromCallback(t *testing.T) {
 	var m *Machine[TestState, TestEvent]
 	callback := func(transition Transition[TestState, TestEvent]) {
 		if transition.Event == testEventId1 {
-			assert.NoError(t, m.Trigger(testEventId2))
+			if m.Trigger(testEventId2) != nil {
+				t.Errorf("Trigger() returned error for event: %v", testEventId2)
+			}
 		}
 	}
 	m = NewMachine(testStateA, testEvents, settings, callback)
@@ -301,8 +398,16 @@ func TestTriggerFromCallback(t *testing.T) {
 		t.Error("NewMachine returned nil")
 	}
 	if m != nil {
-		assert.True(t, m.Can(testEventId1))
-		assert.NoError(t, m.Trigger(testEventId1))
-		assert.Equal(t, testStateD, m.Get())
+		if !m.Can(testEventId1) {
+			t.Errorf("Can() true expected for event: %v", testEventId1)
+		}
+
+		if m.Trigger(testEventId1) != nil {
+			t.Errorf("Trigger() returned error for event: %v", testEventId1)
+		}
+
+		if m.Get() != testStateD {
+			t.Errorf("Invalid state after Trigger(): %v", m.Get())
+		}
 	}
 }
